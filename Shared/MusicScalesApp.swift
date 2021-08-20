9887265ec1fa39c7637054fe5c0f36dc67feb23b
Łukasz Dziedzic <dziedzic@alfabety.pl>
2021-08-20 12:42:39 +0200

    Initial Commit

---
+++ Shared/MusicScalesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicScalesApp.swift
+//  Shared
+//
+//  Created by Łukasz Dziedzic on 20/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct MusicScalesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
